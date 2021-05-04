bb8341c798f92af0622b676039d08a3a33f17b6a
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-05-04 23:47:55 +0100

    Adding missile skeleton code

---
+++ PointDefence/game objects/missile.c
@@ -0,0 +1,21 @@
+#include "pd_core.h"
+
+float CalculateAngle()
+{
+}
+
+float CalculateRotation()
+{
+}
+
+Vector2 CalculateTrajectory()
+{
+}
+
+void check_collision()
+{
+}
+
+void check_base_hit()
+{
+}
