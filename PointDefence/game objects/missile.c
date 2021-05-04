124934a82fc8e5fd4b5dff3acc03f87a700fe7b6
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-05-04 23:54:01 +0100

    Adding maths logic for missile

--- PointDefence/game objects/missile.c
+++ PointDefence/game objects/missile.c
@@ -1,15 +1,22 @@
 #include "pd_core.h"
+#include <math.h>
 
-float CalculateAngle()
+float CalculateAngle(Missile *missile)
 {
+    return atan(missile->targetXDistance / missile->targetYDistance);
 }
 
-float CalculateRotation()
+void CalculateRotation(Missile *missile)
 {
+    int baseRotation = 90;
+    // Check if allied to invert...
+    missile->roatation = baseRotation + (atan2(missile->targetYDistance, missile->targetXDistance) * (180 / 3.14f));
 }
 
-Vector2 CalculateTrajectory()
+Vector2 CalculateTrajectory(Missile *missile)
 {
+    float angle = CalculateAngle(missile);
+    missile->increments = (Vector2){sin(angle), cos(angle)};
 }
 
 void check_collision()
