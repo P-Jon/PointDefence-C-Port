4b33d852311968f0b7b3b848d26974e63276b6c2
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-05-06 23:53:38 +0100

    Removing example code

--- PointDefence/core/game.c
+++ PointDefence/core/game.c
@@ -3,10 +3,7 @@
 void game_loop(){
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+
     }
 }
 
