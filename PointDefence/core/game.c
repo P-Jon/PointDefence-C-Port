a4a4b321ba8067d65271007ebb437606781d967b
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-05-07 23:25:09 +0100

    Setting up game loop

--- PointDefence/core/game.c
+++ PointDefence/core/game.c
@@ -1,13 +1,30 @@
 #include "pd_core.h"
+#include <stdlib.h>
 
-void game_loop(){
+void game_loop()
+{
+    Player *player = malloc(sizeof(Player));
+    GameData *gameData = malloc(sizeof(GameData));
+
+    Missile *missileList = NULL;
+    Explosion *explosionList = NULL;
+
     while (!WindowShouldClose())
     {
-
+        if (IsWindowFocused())
+        {
+        }
     }
 }
 
-void reset_data(Player *player, GameData *gameData, Missile *missiles){
+void PollInput()
+{
+    BeginDrawing();
+    EndDrawing();
+}
+
+void reset_data(Player *player, GameData *gameData, Missile *missiles)
+{
     reset_player_data(player);
 
     gameData->Gameover = false;
